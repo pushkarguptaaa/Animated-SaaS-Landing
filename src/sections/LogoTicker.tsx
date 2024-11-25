679f5bf17306531033b985ad9539555997ad679e
Pushkar Gupta <pushkardps8@gmail.com>
2024-11-25 15:54:37 +0530

    Created logo ticker section

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,25 @@
+import acmeLogo from "../assets/logo-acme.png"
+import quantumLogo from "../assets/logo-quantum.png"
+import echoLogo from "../assets/logo-echo.png"
+import celestialLogo from "../assets/logo-celestial.png"
+import pulseLogo from "../assets/logo-pulse.png"
+import apexLogo from "../assets/logo-apex.png"
+import Image from "next/image"
+
 export const LogoTicker = () => {
-  return null;
+  return <div className="py-8 md:py-12 bg-white">
+    <div className="container">
+      <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+        <div className="flex gap-14 flex-none">
+          <Image className="logo-ticker-image" src={acmeLogo} alt="Acme logo"/>
+          <Image className="logo-ticker-image" src={quantumLogo} alt="Quantum logo"/>
+          <Image className="logo-ticker-image" src={echoLogo} alt="Echo logo"/>
+          <Image className="logo-ticker-image" src={celestialLogo} alt="Celestial logo"/>
+          <Image className="logo-ticker-image" src={pulseLogo} alt="Pulse logo"/>
+          <Image className="logo-ticker-image" src={apexLogo} alt="Apex logo"/>
+        </div>
+      </div>
+      
+    </div>
+  </div>
 };
