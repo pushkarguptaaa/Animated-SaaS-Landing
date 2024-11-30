532ba22d7af0a079f4efa98f105a9ff83e28720e
Pushkar Gupta <pushkardps8@gmail.com>
2024-11-30 14:30:41 +0530

    Created product section animations

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,7 +4,7 @@ import ArrowIcon from "../assets/arrow-right.svg"
 import cogImage from "../assets/cog.png"
 import cylinderImage from "../assets/cylinder.png"
 import noodleImage from "../assets/noodle.png"
-import { motion, useScroll, useTransform, useMotionValueEvent } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 
 export const Hero = () => {
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,11 +1,22 @@
+"use client"
 import Image from "next/image";
 import productImage from "../assets/product-image.png"
 import pyramidImage from "../assets/pyramid.png"
 import tubeImage from "../assets/tube.png"
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const ProductShowcase = () => {
+  const sectionRef = useRef(null)
+  const {scrollYProgress} = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"]
+  })
+
+  const translateY = useTransform(scrollYProgress, [0,1], [150,-150])
+
   return (
-    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
+    <section ref={sectionRef} className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
         <div className="section-heading">
 
@@ -22,8 +33,14 @@ export const ProductShowcase = () => {
 
         <div className="relative">
           <Image src={productImage} alt="Product Image" className="mt-10"/>
-          <Image src={pyramidImage} height={262} width={262} alt="Pyramid image" className="hidden md:block absolute -right-36 -top-32"/>
-          <Image src={tubeImage} height={248} alt="Tube image" className="hidden md:block absolute bottom-24 -left-36"/>
+          <motion.img src={pyramidImage.src} height={262} width={262} alt="Pyramid image" className="hidden md:block absolute -right-36 -top-32"
+          style={{
+            translateY: translateY
+          }}/>
+          <motion.img src={tubeImage.src} height={248} width={248} alt="Tube image" className="hidden md:block absolute bottom-24 -left-36"
+          style={{
+            translateY: translateY
+          }}/>
         </div>
           
         
